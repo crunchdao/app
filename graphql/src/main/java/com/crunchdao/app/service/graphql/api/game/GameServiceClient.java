a80c50c1992ddc621853a9c390570221715d344f
Caceresenzo <caceresenzo1502@gmail.com>
2022-08-26 00:33:19 +0200

    feat(graphql): added achievements

---
+++ graphql/src/main/java/com/crunchdao/app/service/graphql/api/game/GameServiceClient.java
@@ -0,0 +1,28 @@
+package com.crunchdao.app.service.graphql.api.game;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("game-service")
+public interface GameServiceClient {
+	
+	@GetMapping("/v1/achievements")
+	List<AchievementDto> listAchievements();
+	
+	@GetMapping("/v1/achievements/{id}")
+	AchievementDto showAchievement(@PathVariable UUID id);
+	
+	@GetMapping("/v1/achievements/categories")
+	List<AchievementCategoryDto> listAchievementCategories();
+	
+	@GetMapping("/v1/achievements/categories/{id}")
+	AchievementCategoryDto showAchievementCategory(@PathVariable UUID id);
+	
+	@GetMapping("/v1/achievements/users/{userId}")
+	List<AchievementUserDto> showAchievementUser(@PathVariable UUID userId);
+	
+}
