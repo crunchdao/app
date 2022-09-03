397d2edd2540fd5742c0dc5c0f16cdfc692ae9ae
Caceresenzo <caceresenzo1502@gmail.com>
2022-09-03 17:56:05 +0200

    fix(avatar-service): scope was wrong

--- avatar-service/src/main/java/com/crunchdao/app/service/avatar/permission/Scopes.java
+++ avatar-service/src/main/java/com/crunchdao/app/service/avatar/permission/Scopes.java
@@ -2,7 +2,7 @@ package com.crunchdao.app.service.avatar.permission;
 
 public interface Scopes {
 	
-	public static final String PREFIX = "api-key.";
+	public static final String PREFIX = "avatar.";
 	
 	public static final String WRITE = PREFIX + "write";
 	
